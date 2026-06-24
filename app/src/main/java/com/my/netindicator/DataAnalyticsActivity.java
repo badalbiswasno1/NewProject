7179f577425699cc2557dda7fdf794d6c072327a
badalbiswasno1 <badalbiswasno1@gmail.com>
2026-06-24 07:30:40 +0530

    Update DataAnalyticsActivity.java

--- app/src/main/java/com/my/netindicator/DataAnalyticsActivity.java
+++ app/src/main/java/com/my/netindicator/DataAnalyticsActivity.java
@@ -1,12 +1,1 @@
-package com.my.netindicator;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class DataAnalyticsActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_analytics);
-    }
-}
